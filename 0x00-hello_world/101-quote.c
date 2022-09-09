264f517487ac78f9b6938dbd8a83b11c0ce52f76
Jaybans10 <jaybans10@yahoo.com>
2022-09-09 05:03:02 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-#include <unistd.h>
-
 /**
 *main - Prints out the last part of a quote in the standard error.
 *
@@ -10,6 +7,6 @@
 int main(void)
 {
 
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n, 58);
 	return (1);
 }
